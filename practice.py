a81a2bd933ecc5379e597ea8fa0ab2c5c637bfa6
songsam <songsaem33@gmail.com>
2026-03-24 11:41:10 +0900

    first commit

--- practice.py
+++ practice.py
@@ -12,3 +12,12 @@ print(5>2)
 print(5>=2)
 print(5<2)
 print(5<=2)
+print(5 == 2)
+print(5 != 2)
+
+print(3<5 and 7<5)
+print(3<5 or 7<5)
+print(not 3<5)
+
+print('c' in 'cat')
+print('c' not in 'cat')
