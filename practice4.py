387eec6baa242b555a26a5d3c17f307e168e80da
songsam <songsaem33@gmail.com>
2026-03-24 12:51:04 +0900

    first commit

---
+++ practice4.py
@@ -0,0 +1,31 @@
+lang = 'PYTHON'
+print(lang)
+print(lang[0])
+print(lang[-1])
+print(lang[ :3])
+print(lang[2:])
+print(lang[:])
+
+num = 3
+print(num)
+num += 2 # num = num + 2
+print(num)
+
+num -= 1
+print(num)
+num *= 2
+print(num)
+num /= 4
+print(num)
+
+snack = '꿀꽈배기'
+print(len(snack))
+
+snack = '''꿀꽈배기는
+너무
+맛있어요'''
+print(snack)
+
+print('-' * 10)
+print('NadoCoding')
+print('*' * 20)
---
+++ practice5.py
@@ -0,0 +1,11 @@
+from inspect import AGEN_CLOSED
+
+
+print('''I have
+3
+books.''')
+
+print('''My age:
+15
+My weight:
+67.3''')
